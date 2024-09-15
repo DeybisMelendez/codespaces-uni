4a12b8eb9d766900993c36949ac9eb8642faef37
deybis <damv2007@gmail.com>
2024-09-15 12:19:23 -0600

    unificacion de scripts shell

--- .devcontainer/autoupdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Obtener y fusionar cambios del repositorio upstream
-GIT="git --git-dir=$PWD/.git --work-tree=$PWD"
-
-$GIT fetch upstream
-$GIT merge upstream/main -m "Actualización $(date +"%Y-%m-%d %H:%M:%S")"
-$GIT push
--- .devcontainer/update.sh
+++ .devcontainer/update.sh
@@ -3,7 +3,12 @@
 # Define el directorio que quieres verificar y configurar como seguro
 SAFE_DIRECTORY="/workspaces/codespaces-uni"
 REPO="https://github.com/DeybisMelendez/codespaces-uni.git"
-
 GIT="git --git-dir=$PWD/.git --work-tree=$PWD"
 $GIT config --global --add safe.directory "$SAFE_DIRECTORY"
 $GIT remote add upstream $REPO
+
+# Obtener y fusionar cambios del repositorio upstream
+$GIT fetch upstream
+COMMIT=$(date +"%Y-%m-%d %H:%M:%S")
+$GIT merge upstream/main -m "$COMMIT"
+$GIT push

b472aec90624bed3e151ff51baa0155870fdd092
deybis <damv2007@gmail.com>
2024-09-15 11:43:08 -0600

    agregando commit a merge

--- .devcontainer/autoupdate.sh
+++ .devcontainer/autoupdate.sh
@@ -3,10 +3,6 @@
 # Obtener y fusionar cambios del repositorio upstream
 GIT="git --git-dir=$PWD/.git --work-tree=$PWD"
 
-$GIT fetch origin
-$GIT rebase origin
 $GIT fetch upstream
-$GIT rebase upstream/main
-$GIT add .
-$GIT commit -m "Actualización $(date +"%Y-%m-%d %H:%M:%S")"
+$GIT merge upstream/main -m "Actualización $(date +"%Y-%m-%d %H:%M:%S")"
 $GIT push

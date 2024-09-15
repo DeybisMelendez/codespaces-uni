5490899d7405102bf1b028de53f0f0a1011778d1
deybis <damv2007@gmail.com>
2024-09-15 08:51:51 -0600

    modificacion del autoupdate.sh

--- .devcontainer/autoupdate.sh
+++ .devcontainer/autoupdate.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Obtener y fusionar cambios del repositorio upstream
-git fetch origin
-git rebase origin
-git fetch upstream
-git rebase upstream/main
+GIT="git --git-dir=$PWD/.git --work-tree=$PWD"
+$GIT fetch origin
+$GIT rebase origin
+$GIT fetch upstream
+$GIT rebase upstream/main
+$GIT add .
+$GIT commit -m "Actualización $(date +"%Y-%m-%d %H:%M:%S")"
+$GIT push

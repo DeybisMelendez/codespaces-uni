692070443f7c5a0679fc2b43f52ad0bac1a3e88c
deybis <damv2007@gmail.com>
2024-09-25 13:37:16 -0600

    eliminado origin branch

--- .devcontainer/autogit.sh
+++ .devcontainer/autogit.sh
@@ -1,9 +1,6 @@
 # Define el comando git con el directorio actual
 GIT="git --git-dir=$PWD/.git --work-tree=$PWD"
 
-# Obtener la rama actual
-BRANCH=$($GIT rev-parse --abbrev-ref HEAD)
-
 # Verificar si hay cambios antes de hacer git add y commit
 if ! $GIT diff --quiet; then
     # Añadir todos los cambios al área de stage, excluyendo archivos temporales o logs
@@ -34,13 +31,8 @@ if ! $GIT diff --quiet; then
     fi
 
     # Hacer push de los cambios a la rama actual y gestionar errores
-    if ! $GIT push origin "$BRANCH"; then
+    if ! $GIT push; then
         echo "Error: No se pudo hacer push de los cambios"
         exit 1
-    else
-        echo "Push realizado con éxito"
     fi
-
-else
-    echo "No hay cambios para commitear"
 fi
